c06f6972401f394e7b469d5121e59dfebd3fb582
Fábio Naspolini <fabio.naspolini@bbmlogistica.com.br>
2020-06-21 23:02:56 -0300

    Adicionando lib angular-auth-oidc-client. Configurando acesso a Keycloak

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,58 @@ import { Component } from '@angular/core';
 
 import { PoMenuItem } from '@po-ui/ng-components';
 
+import { OnDestroy, OnInit } from '@angular/core'; // angular-auth-oidc-client import
+import { OidcSecurityService } from 'angular-auth-oidc-client'; // angular-auth-oidc-client import
+import { Observable } from 'rxjs'; // angular-auth-oidc-client import
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-
+export class AppComponent implements OnInit {
   readonly menus: Array<PoMenuItem> = [
     // { label: 'Início', action: this.onClick.bind(this), icon: 'po-icon-home', shortLabel: 'Início' },
-    { label: 'Início', link: '/home', icon: 'po-icon-home', shortLabel: 'Início' },
-    { label: 'Cadastro de Usuários', link: '/usuario', icon: 'po-icon-users', shortLabel: 'Usuários'}
+    {
+      label: 'Início',
+      link: '/home',
+      icon: 'po-icon-home',
+      shortLabel: 'Início',
+    },
+    {
+      label: 'Cadastro de Usuários',
+      link: '/usuario',
+      icon: 'po-icon-users',
+      shortLabel: 'Usuários',
+    },
+    {
+      label: 'Sair',
+      action: this.logout.bind(this),
+      icon: 'po-icon-exit',
+      shortLabel: 'Sair',
+    },
   ];
+
+  constructor(public oidcSecurityService: OidcSecurityService) {}
 
   private onClick() {
     alert('Clicked in menu item');
+  }
+
+  ngOnInit() {
+    this.oidcSecurityService.checkAuth().subscribe((auth) => {
+      console.log('is authenticated', auth);
+      if (!auth) {
+        this.login();
+      }
+    });
+  }
+
+  login() {
+    this.oidcSecurityService.authorize();
+  }
+
+  logout() {
+    this.oidcSecurityService.logoff();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,50 @@ import { RouterModule } from '@angular/router';
 import { CadUsuarioComponent } from './pages/usuarios/cad-usuario/cad-usuario.component';
 import { HomeComponent } from './pages/home/home.component';
 
+import { HttpClientModule } from '@angular/common/http'; // angular-auth-oidc-client import
+import { APP_INITIALIZER } from '@angular/core'; // angular-auth-oidc-client import
+import {
+  AuthModule,
+  LogLevel,
+  OidcConfigService,
+} from 'angular-auth-oidc-client'; // angular-auth-oidc-client import
+
+// angular-auth-oidc-client
+export function configureAuth(oidcConfigService: OidcConfigService) {
+  return () =>
+    oidcConfigService.withConfig({
+      // Keycloak Realm URL. Usado para obter configuração do serviço.
+      // Library faz get em http://localhost:8080/auth/realms/master/.well-known/openid-configuration
+      stsServer: 'http://localhost:8080/auth/realms/master',
+      redirectUrl: window.location.origin,
+      postLogoutRedirectUri: window.location.origin,
+      clientId: 'portinari-ui-sample',
+      scope: 'openid profile email',
+      responseType: 'code',
+      silentRenew: true,
+      silentRenewUrl: `${window.location.origin}/silent-renew.html`,
+      logLevel: LogLevel.Debug,
+    });
+}
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    CadUsuarioComponent,
-    HomeComponent
-  ],
+  declarations: [AppComponent, CadUsuarioComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     PoModule,
-    RouterModule.forRoot([])
+    RouterModule.forRoot([]),
+    AuthModule.forRoot(), // oidc
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    OidcConfigService, // oidc
+    {
+      provide: APP_INITIALIZER,
+      useFactory: configureAuth,
+      deps: [OidcConfigService],
+      multi: true,
+    }, // oidc
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
